0c4d0e8eff855a9c1cb6082f70bf6634e3fc30d4
publicENEMY <bukan.ijam@gmail.com>
2013-12-14 13:18:24 +0800

    Initial commit

--- DataTransferPrototype/SilverlightClient/MainPage.xaml.cs
+++ DataTransferPrototype/SilverlightClient/MainPage.xaml.cs
@@ -9,14 +9,223 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using Eneter.Messaging.EndPoints.StringMessages;
+using Eneter.Messaging.EndPoints.TypedMessages;
+using Eneter.Messaging.MessagingSystems.MessagingSystemBase;
+using Eneter.Messaging.MessagingSystems.TcpMessagingSystem;
 
 namespace SilverlightClient
 {
-    public partial class MainPage : UserControl
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-    }
+	public partial class MainPage : UserControl
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+#region Old
+		// The method is called when a message from the desktop application is received.
+		private void ResponseReceived(object sender, TypedResponseReceivedEventArgs<byte[]> e)
+		{
+			textBox2.Text = GetString(e.ResponseMessage);
+		}
+
+		// The method is called when the button to send message is clicked.
+		private void SendMessage_Click(object sender, RoutedEventArgs e)
+		{
+			// Create message sender sending request messages of type Person and receiving responses of type string.
+			IDuplexTypedMessagesFactory aTypedMessagesFactory = new DuplexTypedMessagesFactory();
+			myMessageSender = aTypedMessagesFactory.CreateDuplexTypedMessageSender<byte[], byte[]>();
+			myMessageSender.ResponseReceived += ResponseReceived;
+
+			// Create messaging based on TCP.
+			IMessagingSystemFactory aMessagingSystemFactory = new TcpMessagingSystemFactory();
+			IDuplexOutputChannel aDuplexOutputChannel = aMessagingSystemFactory.CreateDuplexOutputChannel("tcp://127.0.0.1:4502/");
+
+			// Attach output channel and be able to send messages and receive response messages.
+			myMessageSender.AttachDuplexOutputChannel(aDuplexOutputChannel);
+
+			myMessageSender.SendRequestMessage(GetBytes(textBox1.Text));
+		}
+		
+		private IDuplexTypedMessageSender<byte[], byte[]> myMessageSender;
+#endregion
+
+#region Common Utilities
+		static byte[] GetBytes(string str)
+		{
+			byte[] bytes = new byte[str.Length * sizeof(char)];
+			System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+			return bytes;
+		}
+
+		static string GetString(byte[] bytes)
+		{
+			char[] chars = new char[bytes.Length / sizeof(char)];
+			System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+			return new string(chars);
+		}
+
+		private void Log(string s)
+		{
+			// check if we not in main thread
+			if (!Dispatcher.CheckAccess())
+			{
+				// call same method in main thread
+				Dispatcher.BeginInvoke(() =>
+				{
+					Log(s);
+				});
+				return;
+			}
+			// in main thread now
+			LogListBox.Items.Add(s);
+		}
+
+#endregion
+		private void Download1MB(object sender, RoutedEventArgs e)
+		{
+			if (InitializeCommandConnection())
+			{
+				CommandMessageSender.SendMessage("Give|Me|Data|1048576");	
+			}
+
+			//test 
+			InitializeWorkerConnection();
+		}
+
+		private IDuplexTypedMessageSender<byte[], byte[]>[] WorkerMessageSender = new IDuplexTypedMessageSender<byte[], byte[]>[256];
+		//private IDuplexTypedMessageSender<byte[], byte[]> WorkerMessageSender;
+		private IDuplexStringMessageSender CommandMessageSender;
+
+		private bool InitializeCommandConnection()
+		{
+			// Create duplex message sender.
+			// It can send messages and also receive messages.
+			IDuplexStringMessagesFactory aStringMessagesFactory = new DuplexStringMessagesFactory();
+			CommandMessageSender = aStringMessagesFactory.CreateDuplexStringMessageSender();
+			CommandMessageSender.ResponseReceived += CommandResponseReceived;
+
+			// Create TCP based messaging.
+			IMessagingSystemFactory aMessaging = new TcpMessagingSystemFactory();
+			IDuplexOutputChannel aDuplexOutputChannel = aMessaging.CreateDuplexOutputChannel("tcp://127.0.0.1:4502/");
+
+			// Attach the duplex output channel to the message sender
+			// and be able to send messages and receive messages.
+			try
+			{
+				CommandMessageSender.AttachDuplexOutputChannel(aDuplexOutputChannel);
+			}
+			catch (Exception e)
+			{
+				Log(e.Message);
+			}
+
+			if (CommandMessageSender.IsDuplexOutputChannelAttached)
+			{
+				Log("Initialized connection.");
+				return true;
+			}
+			else
+			{
+				Log("Unable to initialize connection");
+				return false;
+			}
+		}
+
+		private void CommandResponseReceived(object sender, StringResponseReceivedEventArgs e)
+		{
+			Log("Received : " + e.ResponseMessage);
+
+			// Analyze command received(received list of server to connect to) ip:port
+			// if download
+			// connect to worker 
+			// ready for download
+			// if upload
+			// use this for upload
+			// Connect to supplied list of server
+			
+			/*
+			var ip = new IPEndPoint[256];
+			ip[0].Address = IPAddress.Loopback;
+			ip[0].Port = 4504;
+			byte[] data = new byte[1048576]; // initialize 1MB data
+			Random random = new Random();
+			random.NextBytes(data);
+
+			if (InitializeWorkerConnection(ip[0]))
+			{
+				WorkerMessageSender[0].SendRequestMessage(data);
+			}
+			*/
+
+			// test disconnect
+			//if(CommandMessageSender.IsDuplexOutputChannelAttached)
+			//	CommandMessageSender.DetachDuplexOutputChannel();
+		}
+
+		private IDuplexOutputChannel Worker4504OutputChannel;
+		private bool InitializeWorkerConnection()
+		{
+			// Create TCP messaging
+			IMessagingSystemFactory aMessaging = new TcpMessagingSystemFactory();
+			Worker4504OutputChannel = aMessaging.CreateDuplexOutputChannel("tcp://127.0.0.1:4504/");
+
+			// Subscribe to response messages.
+			Worker4504OutputChannel.ConnectionClosed += Worker4504OutputChannel_ConnectionClosed;
+			Worker4504OutputChannel.ConnectionOpened += Worker4504OutputChannel_ConnectionOpened;
+			Worker4504OutputChannel.ResponseMessageReceived += Worker4504OutputChannel_ResponseMessageReceived;
+
+			// Open connection and be able to send messages and receive response messages.
+			Worker4504OutputChannel.OpenConnection();
+			Log("Channel id : " + Worker4504OutputChannel.ChannelId);
+
+			// Send a message.
+			byte[] data = new byte[1048576]; // initialize 1MB data
+			//byte[] data = new byte[10]; // initialize 1MB data
+			Random random = new Random();
+			random.NextBytes(data);
+			Worker4504OutputChannel.SendMessage(data);
+			Log("Sent data length : " + data.Length);
+
+			// Close connection.
+			//Worker4504OutputChannel.CloseConnection();
+
+			return true;
+		}
+
+		void Worker4504OutputChannel_ResponseMessageReceived(object sender, DuplexChannelMessageEventArgs e)
+		{
+			Log("Received data length : " + (e.Message as byte[]).Length);
+			Log("ChannelId : " + e.ChannelId);
+
+			//string s = BitConverter.ToString(e.Message as byte[]);
+			//Log("Received : " + s);
+			//Log("Received : " + e.Message.ToString());
+			//Log("Received : " + BitConverter.ToString(e.Message as byte[]));
+			//Worker4504OutputChannel.SendMessage(e.Message);
+		}
+
+		void Worker4504OutputChannel_ConnectionOpened(object sender, DuplexChannelEventArgs e)
+		{
+			Log("Worker 4504 connected");
+			Log("ChannelId : " + e.ChannelId);
+			Log("ResponseReceiverId : " + e.ResponseReceiverId);
+			Log("SenderAddress : " + e.SenderAddress);
+
+			//send to server connection id
+			CommandMessageSender.SendMessage("ResponseReceiverId|Open|" + e.ResponseReceiverId + "|");	
+		}
+
+		void Worker4504OutputChannel_ConnectionClosed(object sender, DuplexChannelEventArgs e)
+		{
+			Log("Worker 4504 disconnected");
+			Log("ChannelId : " + e.ChannelId);
+			Log("ResponseReceiverId : " + e.ResponseReceiverId);
+			Log("SenderAddress : " + e.SenderAddress);
+
+			//notify server closed connection id
+			CommandMessageSender.SendMessage("ResponseReceiverId|Closed|" + e.ResponseReceiverId + "|");
+		}
+	}
 }
--- DataTransferPrototype/WpfServer/MainWindow.xaml.cs
+++ DataTransferPrototype/WpfServer/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,204 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Eneter.Messaging.EndPoints.StringMessages;
+using Eneter.Messaging.EndPoints.TypedMessages;
+using Eneter.Messaging.MessagingSystems.MessagingSystemBase;
+using Eneter.Messaging.MessagingSystems.TcpMessagingSystem;
+using NLog;
 
 namespace WpfServer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-    }
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private ObservableCollection<string> _messages = new ObservableCollection<string>();
+		private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+		private const string TargetName = "memoryex";
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			var target =
+				LogManager.Configuration.AllTargets
+				.Where(x => x.Name == TargetName)
+				.Single() as MemoryTargetEx;
+
+			if (target != null)
+				target.Messages.Subscribe(msg => Dispatcher.InvokeAsync(() =>
+				{
+					_messages.Add(msg);
+				}));
+
+			Logger.Info("Initialized");
+		}
+
+		public ObservableCollection<string> IncomingMessages
+		{
+			get { return _messages; }
+			private set { _messages = value; }
+		}
+
+		private TcpPolicyServer myPolicyServer = new TcpPolicyServer();
+		private IDuplexStringMessageReceiver CommandReceiver;
+
+		//private IDuplexTypedMessageReceiver<byte[],byte[]> Worker4504Receiver;
+		private IDuplexInputChannel Worker4504InputChannel;
+
+		public void StartServer()
+		{
+			// Start the policy server to be able to communicate with silverlight.
+			myPolicyServer.StartPolicyServer();
+
+			// Create duplex message receiver.
+			// It can receive messages and also send back response messages.
+			IDuplexStringMessagesFactory aStringMessagesFactory = new DuplexStringMessagesFactory();
+			CommandReceiver = aStringMessagesFactory.CreateDuplexStringMessageReceiver();
+			CommandReceiver.ResponseReceiverConnected += ClientConnected;
+			CommandReceiver.ResponseReceiverDisconnected += ClientDisconnected;
+			CommandReceiver.RequestReceived += MessageReceived;
+
+			// Create TCP based messaging.
+			IMessagingSystemFactory aMessaging = new TcpMessagingSystemFactory();
+			IDuplexInputChannel aDuplexInputChannel = aMessaging.CreateDuplexInputChannel("tcp://127.0.0.1:4502");
+
+			// Attach the duplex input channel to the message receiver and start listening.
+			// Note: Duplex input channel can receive messages but also send messages back.
+			CommandReceiver.AttachDuplexInputChannel(aDuplexInputChannel);
+			Logger.Info("Server started");
+
+			StartWorker4504Server();
+		}
+
+		public void StartWorker4504Server()
+		{
+			// Create TCP based messaging.
+			IMessagingSystemFactory aMessaging = new TcpMessagingSystemFactory();
+			Worker4504InputChannel = aMessaging.CreateDuplexInputChannel("tcp://127.0.0.1:4504");
+			Worker4504InputChannel.MessageReceived += Worker4504InputChannel_MessageReceived;
+			Worker4504InputChannel.ResponseReceiverConnected += ClientConnected;
+			Worker4504InputChannel.ResponseReceiverDisconnected += ClientDisconnected;
+			//Worker4504InputChannel.ResponseReceiverConnected += Worker4504InputChannel_ResponseReceiverConnected;
+			//Worker4504InputChannel.ResponseReceiverDisconnected += Worker4504InputChannel_ResponseReceiverDisconnected;
+			Worker4504InputChannel.StartListening();
+
+			Logger.Info("Worker server 4504 started");
+		}
+
+		void Worker4504InputChannel_MessageReceived(object sender, DuplexChannelMessageEventArgs e)
+		{
+			// echo back
+			//Logger.Info(BitConverter.ToString(e.Message as byte[]));
+			//string s = BitConverter.ToString(e.Message as byte[]);
+			//Logger.Info("Received : " + s);
+			Logger.Info("Received data length : " + (e.Message as byte[]).Length);
+			//Logger.Info(e.Message.ToString());
+			Worker4504InputChannel.SendResponseMessage(e.ResponseReceiverId, e.Message);
+		}
+
+		public void StopServer()
+		{
+			// Close listenig.
+			// Note: If the listening is not closed, then listening threads are not ended
+			//       and the application would not be closed properly.
+			CommandReceiver.DetachDuplexInputChannel();
+			Worker4504InputChannel.StopListening();
+
+			myPolicyServer.StopPolicyServer();
+		}
+
+		// The method is called when a message from the client is received.
+		private void MessageReceived(object sender, StringRequestReceivedEventArgs e)
+		{
+			Logger.Info("Received : " + e.RequestMessage);
+
+			// Analyze message
+				// split strings
+				// receiving responsereceiverid from client
+			// Calculate mtu
+			// Warm up stopwatch
+			// Send message
+			CommandReceiver.SendResponseMessage(e.ResponseReceiverId, e.RequestMessage);
+		}
+
+		private void Window_Closed(object sender, EventArgs e)
+		{
+			StopServer();
+		}
+
+		// The method is called when a client is connected.
+		// The Silverlight client is connected when the client attaches the output duplex channel.
+		private void ClientConnected(object sender, ResponseReceiverEventArgs e)
+		{
+			// Add the connected client to the listbox.
+			Dispatcher.InvokeAsync(() =>
+			{
+				ConnectedClientsListBox.Items.Add(e.ResponseReceiverId);
+			});
+			Logger.Info("ResponseReceiverId : " + e.ResponseReceiverId);
+			Logger.Info("SenderAddress : " + e.SenderAddress);
+		}
+
+		// The method is called when a client is disconnected.
+		// The Silverlight client is disconnected if the web page is closed.
+		private void ClientDisconnected(object sender, ResponseReceiverEventArgs e)
+		{
+			// Remove the disconnected client from the listbox.
+			Dispatcher.InvokeAsync(() =>
+			{
+				ConnectedClientsListBox.Items.Remove(e.ResponseReceiverId);
+			});
+			Logger.Info("ResponseReceiverId : " + e.ResponseReceiverId);
+			Logger.Info("SenderAddress : " + e.SenderAddress);
+		}
+
+		private void BroadcastMessage(string s)
+		{
+			// Send the message to all connected clients.
+			foreach (string aClientId in ConnectedClientsListBox.Items)
+			{
+				CommandReceiver.SendResponseMessage(aClientId, s);
+			}
+		}
+
+		static byte[] GetBytes(string str)
+		{
+			byte[] bytes = new byte[str.Length * sizeof(char)];
+			System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+			return bytes;
+		}
+
+		static string GetString(byte[] bytes)
+		{
+			char[] chars = new char[bytes.Length / sizeof(char)];
+			System.Buffer.BlockCopy(bytes, 0, chars, 0, bytes.Length);
+			return new string(chars);
+		}
+
+
+		private void Server_Click(object sender, RoutedEventArgs e)
+		{
+			if (CommandReceiver == null)
+			{
+				StartServer();
+				var s = sender as Button;
+				s.Content = "Stop Server";
+			}
+			else if (CommandReceiver.IsDuplexInputChannelAttached)
+			{
+				StopServer();
+				var s = sender as Button;
+				s.Content = "Start Server";
+			}
+			else
+			{
+				StartServer();
+				var s = sender as Button;
+				s.Content = "Stop Server";
+			}
+		}
+	}
 }
